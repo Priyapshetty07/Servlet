c14b5fd68d04e25307734c8c68c6b40ec67808da
Rakesh <you@example.com>
2021-10-30 11:28:49 +0530

    Servlet demo

---
+++ ServletDemo/src/main/java/com/xworkz/web/servlet/WelcomeServlet.java
@@ -0,0 +1,11 @@
+package com.xworkz.web.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+public class WelcomeServlet extends HttpServlet {
+
+	public WelcomeServlet() {
+		System.out.println(this.getClass().getName() + "Created");
+	}
+
+}
